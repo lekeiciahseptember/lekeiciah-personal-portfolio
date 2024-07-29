5d8c879c0fdefcaec735883262e824c667bb59ee
Lekeiciah September <lekeiciahanuskas@gmail.com>
2024-07-29 15:22:57 +0200

    adding file

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css"
+
+function Home() {
+    return (
+        <body>
+            <div className = "container">
+                <h1>Hi, my name is</h1>
+                <h1>Lekeiciah.</h1>
+            </div>
+        </body>
+    )
+}
+export default Home;
