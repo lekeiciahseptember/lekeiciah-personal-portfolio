d4d824d3b00720a709d577267e34dd1b648e4bc1
Lekeiciah September <lekeiciahanuskas@gmail.com>
2024-08-05 23:11:02 +0200

    added button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home";
+import Info from "./components/Info";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={< Home />} />
+      <Route path="/info" element={< Info />} />
     </Routes>
     </BrowserRouter>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "./Home.css"
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+
+    const navigate = useNavigate();
+
+    const RedirectViewMore = () => {
+        navigate("/Info")
+    }
     return (
         <body>
             <div className = "container">
                 <h1>Hi, my name is</h1>
                 <h1>Lekeiciah.</h1>
             </div>
+
+            <div>
+                <button onClick={RedirectViewMore}>View More</button>
+            </div>
         </body>
     )
 }
 export default Home;
+
+
